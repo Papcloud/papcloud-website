3c3e683c9703d45982c4690c82664e1f49392f01
mohan saravanan <mohan@papcloud.com>
2024-12-23 16:33:44 +0000

    asding the lock file

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,7 +4,9 @@ function Home() {
   return (
     <div>
       <div>
-        <h2>Papcloud - Photograph, AI, Production and Cloud</h2>
+        <h2>
+          Papcloud is Photograph, AI Production and Cloud consulting company
+        </h2>
         <p>We help businesses grow through creative digital solutions.</p>
       </div>
       <div>
